a04b989758c9c9ac51bff7a5adfcec10d341c28e
tinghao.ma <tinghao.ma@ele.me>
2019-04-25 22:09:50 +0800

    minimist 解析参数

--- minimist/minimist.js
+++ minimist/minimist.js
@@ -1,4 +1,16 @@
 var argv = require('minimist')(process.argv.slice(2));
-console.dir(argv);
 
-//npm run mm -a beep -b boop
+console.log(argv['platform'])
+console.log(argv['env'])
+
+
+
+var argv2 = require('minimist')(process.argv.slice(2),{
+    string:["platform"],
+    env:["env"]
+});
+console.log(argv2.platform)
+console.log(argv2.env)
+
+
+// npm run nn -- --platform=alipay --env=pro
